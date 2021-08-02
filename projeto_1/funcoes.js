6cc8123e03f712355b2ddca6f56ef78d0e52e6f9
João Pedro <joaomljcanal@gmail.com>
2021-08-02 10:11:58 -0300

    projeto #01 - palavras mais usadas

---
+++ projeto_1/funcoes.js
@@ -0,0 +1,107 @@
+const path = require('path')
+const fs = require('fs')
+
+function lerDiretorio(caminho) {
+  return new Promise((resolve, reject) => {
+    try {
+      const arquivos = fs.readdirSync(caminho)
+      const arquivosCompletos = arquivos.map((arquivo) =>
+        path.join(caminho, arquivo)
+      )
+      resolve(arquivosCompletos)
+    } catch (err) {
+      reject(err)
+    }
+  })
+}
+
+function lerArquivo(caminho) {
+  return new Promise((resolve, reject) => {
+    try {
+      const conteudo = fs.readFileSync(caminho, { encoding: 'utf-8' })
+      resolve(conteudo.toString())
+    } catch (err) {
+      reject(err)
+    }
+  })
+}
+
+function lerArquivos(caminhos) {
+  return Promise.all(caminhos.map((caminho) => lerArquivo(caminho)))
+}
+
+function elementosTerminadosCom(padrao) {
+  return function (array) {
+    return array.filter((el) => el.endsWith(padrao))
+  }
+}
+
+function removerElementosSeVazio(array) {
+  return array.filter((el) => el.trim())
+}
+
+function removerElementosSeIncluir(padraoTextual) {
+  return function (array) {
+    return array.filter((el) => !el.includes(padraoTextual))
+  }
+}
+
+function removerElementosSeApenasNumero(array) {
+  return array.filter((el) => {
+    const num = parseInt(el.trim())
+    return num !== num
+  })
+}
+
+function removerSimbolos(simbolos) {
+  return function (array) {
+    return array.map((el) => {
+      return simbolos.reduce((acc, simbolo) => {
+        return acc.split(simbolo).join('')
+      }, el)
+    })
+  }
+}
+
+function mesclarElementos(array) {
+  return array.join(' ')
+}
+
+function separarTextoPor(simbolo) {
+  return function (texto) {
+    return texto.split(simbolo)
+  }
+}
+
+function agruparElementos(palavras) {
+  return Object.values(
+    palavras.reduce((acc, palavra) => {
+      const el = palavra.toLowerCase()
+      const qtde = acc[el] ? acc[el].qtde + 1 : 1
+      acc[el] = { elemento: el, qtde }
+      return acc
+    }, {})
+  )
+}
+
+function ordernarPorAtribNumerico(attr, ordem = 'asc') {
+  return function (array) {
+    const asc = (o1, o2) => o1[attr] - o2[attr]
+    const desc = (o1, o2) => o2[attr] - o1[attr]
+    return array.sort(ordem === 'asc' ? asc : desc)
+  }
+}
+
+module.exports = {
+  lerDiretorio,
+  elementosTerminadosCom,
+  lerArquivos,
+  removerElementosSeVazio,
+  removerElementosSeIncluir,
+  removerElementosSeApenasNumero,
+  removerSimbolos,
+  mesclarElementos,
+  separarTextoPor,
+  agruparElementos,
+  ordernarPorAtribNumerico
+}
