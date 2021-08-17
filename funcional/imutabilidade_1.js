1ccba9b7f50046865067f058cde5fb7c91d780ce
João Pedro <joaomljcanal@gmail.com>
2021-08-16 21:10:33 -0300

    imutabilidade #01

---
+++ funcional/imutabilidade_1.js
@@ -0,0 +1,18 @@
+// Irá modificar a array original
+
+// function ordenar(array) {
+//   return array.sort()
+// }
+
+// Não irá modificar a array original
+function ordenar(array) {
+  return [...array].sort((a, b) => a - b)
+}
+
+const nums = Object.freeze([3, 1, 7, 9, 4, 6])
+
+const numsOrdenados = ordenar(nums)
+console.log(nums, numsOrdenados)
+
+const parteNums = nums.slice(1)
+console.log(parteNums, nums)
