10159dd12afb868e0b444f0ee19ce872864bae32
João Pedro <joaomljcanal@gmail.com>
2021-08-23 11:46:09 -0300

    observables

---
+++ rxjs/observable_3.js
@@ -0,0 +1,12 @@
+const { Observable } = require('rxjs')
+
+function entre(min, max) {
+  return new Observable((subscriber) => {
+    for (let i = min; i <= max; i++) {
+      subscriber.next(i)
+    }
+    subscriber.complete()
+  })
+}
+
+entre(4, 10).subscribe((num) => console.log(`num = ${num}`))
