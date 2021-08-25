4e1767e1b823b922111e4386ca74be181baddd71
João Pedro <joaomljcanal@gmail.com>
2021-08-25 11:42:18 -0300

    subscription

---
+++ rxjs/subscription.js
@@ -0,0 +1,19 @@
+import { Subscription, timer } from "rxjs";
+
+const obs = timer(3000, 500);
+
+const sub1 = obs.subscribe((num) => {
+  console.log(`#1 Gerou o número ${num}`);
+});
+
+const sub2 = obs.subscribe((num) => {
+  console.log(`#2 Gerou o número ${num}`);
+});
+
+const sub = new Subscription();
+sub.add(sub1);
+sub.add(sub2);
+
+setTimeout(() => {
+  sub.unsubscribe();
+}, 5000);
